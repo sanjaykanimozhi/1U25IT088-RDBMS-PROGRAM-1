eb57a36b53346de0738e82f1eb7660bd108f736a
Sanjay <sanjaykanimozhi58@gmail.com>
2026-08-20 13:48:58 +0530

    1U25IT088 PR0 1 RDBMS

--- solution.sql
+++ solution.sql
@@ -1,1 +1,12 @@
+CREATE DATABASE cOLLEGEDB;
+
+USE CollegeDB;
+
+CREATE TABLE Department (
+    DepartmentID NUMERIC(5) PRIMARY KEY,
+    DepartmentName VARCHAR(20) NOT NULL,
+    HOD VARCHAR(20) NOT NULL
+);
+
+DESCRIBE Department;
 
